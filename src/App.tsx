44928d0cfd83cd1a896c2cca2f92bca3a3b4117b
Zuraiz Zafar <zuraiz.zafar97@gmail.com>
2019-09-08 01:36:57 -0400

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './App.scss';
+
+
+const fibonacci = ['1', '2', '3', '5', '8', '13', '21', '34', '55', '89', '☕', '∞']
 
 const App: React.FC = () => {
+  const [selected, setSelected] = useState('');
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <h1>Scrum Poker</h1>
+      <div className="card-container">
+        {fibonacci.map(value =>
+          <Card hidden={selected !== value && selected !== ''} selected={selected === value} onClick={() => { selected === value ? setSelected('') : setSelected(value) }} value={value} key={value} />
+        )}
+      </div>
     </div>
   );
+}
+
+const Card: React.FC<{
+  value: string,
+  selected: boolean,
+  hidden: boolean
+  onClick: (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => void
+}> = ({ value, onClick, selected, hidden }) => {
+  return (
+    <div onClick={onClick} className={`card ${selected && 'selected'} ${hidden && 'hidden'}`} >
+      {value}
+    </div >
+  )
 }
 
 export default App;
